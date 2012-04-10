093827f0260c2f43931cfa53c5a1fe38b9b9ee94
Gianfranco <gfcs.798@gmail.com>
2012-04-09 21:24:04 -0500

    Devuelve cantidad dinamicamente

--- romanNumerals.rb
+++ romanNumerals.rb
@@ -3,23 +3,24 @@ attr_reader :I
 attr_reader :V
 attr_reader :X
 attr_reader :resultado
-def convertir(cadena) 
-	@I = 1
-	@V = 5
-	@X = 10
-	if cadena=="I"
-		@resultado=1
+	def convertir(cadena) 
+		@I = 1
+		@V = 5
+		@X = 10
+	    @resultado = 0
+		
+		index = cadena.length-1
+		#puts "value " +cadena[0]
+		while index >= 0	
+			
+			if cadena[index]=="I"
+				
+				@resultado=@resultado+1
+			end
+			index = index -1
+		end
+
+		@resultado
 	end
-	if cadena=="II"
-		@resultado=2
-	end
-	if cadena=="III"
-		@resultado=3
-	end
-	#cadena.length.downto(0).do |i|
-	#	puts i
-	#end
-	@resultado
 end
 
-end
--- romanNumerals_spec.rb
+++ romanNumerals_spec.rb
@@ -7,7 +7,13 @@ describe RomanNumerals do
     end
     context "Probando Numeros" do
 		it "Numero es I" do		
-			@romanNumerals.convertir("I").should==1 
+			@romanNumerals.convertir("I").should==1
+		end
+		it "Numero es II" do		
+			@romanNumerals.convertir("II").should==2
+		end
+		it "Numero es III" do		
+			@romanNumerals.convertir("III").should==3
 		end
 	end
 
