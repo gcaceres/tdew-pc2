396cb4c3f0ed7bd63c3960b1f496ff122f551b54
Gianfranco <gfcs.798@gmail.com>
2012-04-09 21:52:23 -0500

    Probando hasta 30

--- romanNumerals_spec.rb
+++ romanNumerals_spec.rb
@@ -71,12 +71,59 @@ describe RomanNumerals do
 		it "Numero es XVII" do		
 			@romanNumerals.convertir("XVII").should==17
 		end
-		
+
 		it "Numero es XVIII" do		
 			@romanNumerals.convertir("XVIII").should==18
 		end
 
+				it "Numero es XIX" do		
+			@romanNumerals.convertir("XIX").should==19
+		end
 
+				it "Numero es XX" do		
+			@romanNumerals.convertir("XX").should==20
+		end
+
+		it "Numero es XXI" do		
+			@romanNumerals.convertir("XXI").should==21
+		end
+
+		it "Numero es XXII" do		
+			@romanNumerals.convertir("XXII").should==22
+		end
+
+		it "Numero es XXIII" do		
+			@romanNumerals.convertir("XXIII").should==23
+		end
+
+
+		it "Numero es XXIV" do		
+			@romanNumerals.convertir("XXIV").should==24
+		end
+
+		it "Numero es XXV" do		
+			@romanNumerals.convertir("XXV").should==25
+		end
+
+		it "Numero es XXVI" do		
+			@romanNumerals.convertir("XXVI").should==26
+		end
+
+		it "Numero es XXVII" do		
+			@romanNumerals.convertir("XXVII").should==27
+		end
+
+		it "Numero es XXVIII" do		
+			@romanNumerals.convertir("XXVIII").should==28
+		end
+
+				it "Numero es XXIX" do		
+			@romanNumerals.convertir("XXIX").should==29
+		end
+
+				it "Numero es XXX" do		
+			@romanNumerals.convertir("XXX").should==30
+		end
 	end
 
 end
