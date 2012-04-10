d695ba21b5c1e93413784c5eab969e13ab554065
Gianfranco <gfcs.798@gmail.com>
2012-04-09 20:57:56 -0500

    Devuelve 1 si es I

--- romanNumerals.rb
+++ romanNumerals.rb
@@ -1,8 +1,18 @@
 class RomanNumerals
-attr_reader :numero
+attr_reader :I
+attr_reader :V
+attr_reader :X
 def convertir(cadena) 
+	:SI = 1
+	:SV = 5
+	:SX = 10
+	if cadena=="I"
+		:S1
+	end
 
-	1
+	#cadena.length.downto(0).do |i|
+	#	puts i
+	#end
 	
 end
 
